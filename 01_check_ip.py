230afd643f4469cf1610c6d6cb695454a7816426
lliliii <105020691+lliliii@users.noreply.github.com>
2022-11-10 20:26:34 +0900

    Update 01_check_ip.py

--- 01_check_ip.py
+++ 01_check_ip.py
@@ -5,7 +5,11 @@ chrome_options = Options()
 chrome_options.add_argument("--proxy-server=socks5://127.0.0.1:9050")
 driver = webdriver.Chrome(executable_path='./chromedriver',options=chrome_options)
 
-#Search
+# ipinfo.io 접속
 driver.get('https://ipinfo.io/json')
+
+# html 소스코드
 html = driver.page_source
+
+# 출력
 print(html)
